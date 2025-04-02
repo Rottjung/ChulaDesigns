0a6e82d4eb8bc98bf7df8d2eae117dd9bbfbdd12
Rottjung <jethrovolders@gmail.com>
2025-04-02 19:17:29 +0700

    Update script.js

--- design2/script.js
+++ design2/script.js
@@ -74,7 +74,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // Add click event listeners to menu items for submenu activation
     menuItems.forEach(item => {
         item.addEventListener('click', function () {
-            circular-Menu.classList.add('submenu-view');
+            item.circularMenu.classList.add('submenu-view');
             // Additional logic to display the corresponding submenu content
         });
     });
@@ -82,7 +82,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // Add click event listeners to back buttons to return to main menu
     backButtons.forEach(button => {
         button.addEventListener('click', function () {
-            circularMenu.classList.remove('submenu-view');
+            item.circularMenu.classList.remove('submenu-view');
             // Additional logic to hide the submenu content
         });
     });
